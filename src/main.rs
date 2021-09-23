9ffaf8c72426a771f1c0edf6a24edb023e735f2c
linux_china <libing.chen@gmail.com>
2021-09-23 00:44:35 -0700

    main.rs

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+#![recursion_limit = "1024"]
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+use console_error_panic_hook::set_once as set_panic_hook;
+use wasm_bindgen::prelude::*;
+use web_sys::window;
+
+fn start_app() {
+    let document = window().and_then(|win| win.document()).expect("Could not access document");
+    let body = document.body().expect("Could not access document.body");
+    let text_node = document.create_text_node("Hello, world from Vanilla Rust!!");
+    body.append_child(text_node.as_ref()).expect("Failed to append text");
+}
+
+#[wasm_bindgen(inline_js = "export function snippetTest() { console.log('Hello from JS FFI!!'); }")]
+extern "C" {
+    fn snippetTest();
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+fn main() {
+    set_panic_hook();
+    snippetTest();
+    start_app();
+}
