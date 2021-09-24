b9e1d60c0d6d51ea13c59f12cb1fa0e837d2c932
linux_china <libing.chen@gmail.com>
2021-09-24 12:30:14 -0700

    Polishing

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,6 @@
  * @type {import('vite').UserConfig}
  */
 const config = {
-    root: "dist",
-    build: {
-        rollupOptions: {
-            output: {
-                assetFileNames: `[name].[ext]`
-            }
-        }
-    }
 }
 
 export default config
